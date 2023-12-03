1dcaa07e6bcb0dd481d6e1c9b0dbfa0120164bd5
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-12-03 15:49:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_02_Foreach_Busca_de_Elemento_em_uma_Lista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            {
+                
+                List<string> palavras = new List<string> { "casa", "carro", "cachorro", "gato" };
+
+                Console.WriteLine("Digite uma palavra:");
+                string palavra = Console.ReadLine();
+
+                bool encontrada = false;
+                foreach (string p in palavras)
+                {
+                    if (p == palavra)
+                    {
+                        encontrada = true;
+                        break;
+                    }
+                }
+
+                if (encontrada)
+                {
+                    Console.WriteLine("A palavra '" + palavra + "' foi encontrada.");
+                }
+                else
+                {
+                    Console.WriteLine("A palavra '" + palavra + "' não foi encontrada.");
+
+                    Console.ReadKey();  
+                }
+            }
+        }
+    }
+}
+   
